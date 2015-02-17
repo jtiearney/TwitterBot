bfefe39361f5c230f06500eaff7e17d752dbe074
jtiearney <justin.tiearney@gmail.com>
2015-02-17 13:59:07 -0800

    Changing search to a status update

--- twitter.py
+++ twitter.py
@@ -1,12 +1,14 @@
 import TwitterAPI import TwitterAPI
 
+#Add your twitter API keys https://dev.twitter.com
 con_key = ''
 con_secret = ''
 access_token_key = ''
 access_token_secret = ''
 
+#Authenticate with Twitter
 twitter = TwitterAPI(con_key, con_secret, access_token_key, access_token_secret)
 
-request = twitter.request('search/tweets', {'q':'vancouver'})
-for item in r:
-	print item
+#Print a simple tweet and return the status code
+request = twitter.request('statuses/update', {'status':'A tweet from Vancouver'})
+print request.status_code
